a8401568eeb37ac4aa9ae8901da02fbba6ad7989
Carlos_Cruceta <c.cruceta.2016@alumnos.urjc.es>
2023-03-09 09:58:02 +0100

    boom

---
+++ P2/BOOM/index.js
@@ -0,0 +1,101 @@
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+//-- Obtener una colección con todos los elementos
+//-- de la clase dígito
+digitos = document.getElementsByClassName("digito")
+operaciones = document.getElementsByClassName("operacion")
+
+//-- Estados de la calculadora
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2: 3,
+  }
+
+  //-- Variable de estado
+//-- Por defecto su valor será el del estado inicial
+let estado = ESTADO.INIT;
+
+//-- Función de retrollamada de los digitos
+function digito(ev)
+{
+    //-- Se ha recibido un dígito
+    //-- Según en qué estado se encuentre la calculadora
+    //-- se hará una cosa u otra
+
+    //-- Si es el primer dígito, no lo añadimos,
+    //-- sino que lo mostramos directamente en el display
+    if (estado == ESTADO.INIT) {
+
+        display.innerHTML = ev.target.value;
+
+        //-- Pasar al siguiente estado
+        estado = ESTADO.OP1;
+
+    } else {
+       
+        //--En cualquier otro estado lo añadimos
+        display.innerHTML += ev.target.value;
+
+        //-- Y nos quedamos en el mismo estado
+        //-- Ojo! Este ejemplo sólo implementa el primer
+        //-- estado del diagrama. Habría que tener en 
+        //-- cuenta el resto... lo debes hacer en tu práctica
+    } 
+    
+}
+
+
+//-- Establecer la misma función de retrollamada
+//-- para todos los botones de tipo dígito
+for (let boton of digitos) {
+
+    //-- Se ejecuta cuando se pulsa un boton
+    //-- que es un dígito. Para que el código sea 
+    //-- mas legible la función de retrollamada se
+    //-- escribe como una función normal (digito)
+    boton.onclick = digito;
+}
+
+//-------- Resto de funciones de retrollamada
+
+//-- Operación de sumar
+suma.onclick = (ev) => {
+
+    //-- Insertar simbolo de sumar
+    display.innerHTML += ev.target.value;
+
+    //-- ¡Ojo! Aquí se inserta el + siempre!
+    //-- Para que la calculadora funcione bien
+    //-- sólo se debe permitir insertar el operador
+    //-- en el estado OP1, y debe cambiar el estado
+    //-- a OPERATION (según el diagrama de estados)
+  
+}
+
+//-- Evaluar la expresión
+igual.onclick = () => {
+  
+    //-- Calcular la expresión y añadirla al display
+    display.innerHTML = eval(display.innerHTML);
+
+    //-- ¡Ojo! Aquí se hace siempre!
+    //-- Sólo se debe permitir que eso se haga
+    //-- si se está en el estado final (OP2)
+  
+}
+
+//-- Poner a cero la expresión
+//-- Y volver al estado inicial
+clear.onclick = () => {
+  display.innerHTML = "0";
+  estado = ESTADO.INIT;
+}
+//-- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//-- Establecer la misma función de retrollamada
+//-- para todos los botones de tipo dígito
--- P3/S6/canvas-animacion1.js
+++ P3/S6/canvas-animacion1.js
@@ -9,28 +9,43 @@ canvas.height = 100;
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
+//-- Valor del lado del rectángulo
+let lr = 20;
 //-- Coordenadas del objeto
 let x = 0;
 let y = 10;
 
-//-- Velocidad horizontal del objeto
-let velx = 1;
+//-- Velocidades del objeto
+let velx = 3;
+let vely = 1;
 
 //-- Función principal de animacion
 function update() 
 {
   console.log("test");
+  
+  //-- Condicion de rebote en extremos horizontales del canvas
+  if (x < 0 || x >= (canvas.width - lr) ) { 
+    velx = -velx;
+  }
+
+  //-- Condición de rebote en extremos verticales del canvas
+  if (y <= 0 || y > canvas.height- lr) {
+    vely = -vely;
+  }
   //-- Algoritmo de animacion:
   //-- 1) Actualizar posicion del  elemento
   //-- (física del movimiento rectilineo uniforme)
+  
   x = x + velx;
+  y = y + vely;
 
   //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //-- 3) Dibujar los elementos visibles
   ctx.beginPath();
-    ctx.rect(x, y, 20, 20);
+    ctx.rect(x, y, lr, lr);
 
     //-- Dibujar
     ctx.fillStyle = 'red';
