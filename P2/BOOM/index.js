b57d6fb4b6b84aed9099a07cd73f2fc40046aee2
CarlosCruceta <c.cruceta.2016@alumnos.urjc.es>
2023-03-20 14:11:38 +0100

    cambio de botones

--- P2/BOOM/index.js
+++ P2/BOOM/index.js
@@ -7,8 +7,7 @@ const gui = {
     stop : document.getElementById("stop"),
     reset : document.getElementById("reset"),
     digitos : document.getElementsByClassName("digito"),
-    clave: document.getElementById("clave"),
-    secretkey: document.getElementById("secret_key")
+    numeros : document.getElementsByClassName("num")
 }
 //-- Clase cronómetro
 class Crono {
@@ -76,14 +75,18 @@ class Crono {
 }
 
 const crono = new Crono(gui.display);
+let check1 = false;
+let check2 = false;
+let check3 = false;
+let check4 = false;
 
 //---- Configurar las funciones de retrollamada
 
 //-- Array que almacena números secretos
-const secretkey = [];
 
 //-- Función que genera la clave secreta aleatoria
 function key(){
+    secretkey = [];
     //-- Generar números aleatorios con un valor máximo
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
@@ -94,12 +97,20 @@ function key(){
         let rnum = getRandomInt(9);
         secretkey.push(rnum.toString());
     }
-    return secretkey.join("-");
+    return secretkey;
 }
+
+function num_correcto(num, secret_key){
+    for(let i = 0; i < secret_key.length; i++){
+        if (num == secret_key[i]){
+            return true;
+        }
+    }
+}
+
 //-- Arranque del cronometro
 gui.start.onclick = () => {
     crono.start();
-    gui.secretkey.innerHTML = key();
     }
       
     //-- Detener el cronómetro
@@ -110,5 +121,150 @@ gui.start.onclick = () => {
     //-- Reset del cronómetro
     gui.reset.onclick = () => {
     crono.reset();
-    gui.secretkey.innerHTML = "****";
+    }
+
+    const ESTADO = {
+        INIT: 0,
+        DIG1: 1,
+        DIG2: 2,
+        DIG3: 3,
+
+      }
+    
+      //-- Variable de estado
+    //-- Por defecto su valor será el del estado inicial
+    let estado = ESTADO.INIT; 
+    let clave_secreta = key();
+    console.log(clave_secreta);
+    //-- Función de retrollamada de los digitos
+    function digito(ev) {
+      
+        if (estado == ESTADO.INIT) {
+
+            crono.start();
+
+            if ((ev.target.value == clave_secreta[0])&(check1 == false)) {
+                console.log("Primer numero correcto");
+                gui.numeros[0].innerHTML = ev.target.value;
+                check1 = true;
+                estado = ESTADO.DIG1
+
+            } else if ((ev.target.value == clave_secreta[1])&(check2 == false)) {
+                console.log("Segundo numero correcto");
+                gui.numeros[1].innerHTML = ev.target.value;
+                check2 = true;
+                estado = ESTADO.DIG1 
+
+            } else if ((ev.target.value == clave_secreta[2])&(check3 == false)) {
+                console.log("Tercer numero correcto");
+                gui.numeros[2].innerHTML = ev.target.value;
+                check3 = true;
+                estado = ESTADO.DIG1
+
+            } else if ((ev.target.value == clave_secreta[3])&(check4 == false)) {
+                console.log("Cuarto numero correcto");
+                gui.numeros[3].innerHTML = ev.target.value;
+                check4 = true;
+                estado = ESTADO.DIG1
+
+            } else {
+                crono.tic();
+            }
+            
+    
+        } else if (estado == ESTADO.DIG1) {
+           
+            if ((ev.target.value == clave_secreta[0])&(check1 == false)) {
+                console.log("Primer numero correcto");
+                gui.numeros[0].innerHTML = ev.target.value;
+                check1 = true;
+                estado = ESTADO.DIG2
+
+            } else if ((ev.target.value == clave_secreta[1])&(check2 == false)) {
+                console.log("Segundo numero correcto");
+                gui.numeros[1].innerHTML = ev.target.value;
+                check2 = true;
+                estado = ESTADO.DIG2 
+
+            } else if ((ev.target.value == clave_secreta[2])&(check3 == false)) {
+                console.log("Tercer numero correcto");
+                gui.numeros[2].innerHTML = ev.target.value;
+                check3 = true;
+                estado = ESTADO.DIG2
+
+            } else if ((ev.target.value == clave_secreta[3])&(check4 == false)) {
+                console.log("Cuarto numero correcto");
+                gui.numeros[3].innerHTML = ev.target.value;
+                check4 = true;
+                estado = ESTADO.DIG2
+
+            } else {
+                crono.tic();
+            }
+    
+        
+        } else if (estado == ESTADO.DIG2) {
+            if ((ev.target.value == clave_secreta[0])&(check1 == false)) {
+                console.log("Primer numero correcto");
+                gui.numeros[0].innerHTML = ev.target.value;
+                check1 = true;
+                estado = ESTADO.DIG3
+
+            } else if ((ev.target.value == clave_secreta[1])&(check2 == false)) {
+                console.log("Segundo numero correcto");
+                gui.numeros[1].innerHTML = ev.target.value;
+                check2 = true;
+                estado = ESTADO.DIG3 
+
+            } else if ((ev.target.value == clave_secreta[2])&(check3 == false)) {
+                console.log("Tercer numero correcto");
+                gui.numeros[2].innerHTML = ev.target.value;
+                check3 = true;
+                estado = ESTADO.DIG3
+
+            } else if ((ev.target.value == clave_secreta[3])&(check4 == false)) {
+                console.log("Cuarto numero correcto");
+                gui.numeros[3].innerHTML = ev.target.value;
+                check4 = true;
+                estado = ESTADO.DIG3
+
+            } else {
+                crono.tic();
+            }
+
+        } else if (estado == ESTADO.DIG3) {
+           
+            if ((ev.target.value == clave_secreta[0])&(check1 == false)) {
+                console.log("Primer numero correcto");
+                gui.numeros[0].innerHTML = ev.target.value;
+                check1 = true;
+                crono.stop();
+
+            } else if ((ev.target.value == clave_secreta[1])&(check2 == false)) {
+                console.log("Segundo numero correcto");
+                gui.numeros[1].innerHTML = ev.target.value;
+                check2 = true;
+                crono.stop();
+
+            } else if ((ev.target.value == clave_secreta[2])&(check3 == false)) {
+                console.log("Tercer numero correcto");
+                gui.numeros[2].innerHTML = ev.target.value;
+                check3 = true;
+                crono.stop();
+
+            } else if ((ev.target.value == clave_secreta[3])&(check4 == false)) {
+                console.log("Cuarto numero correcto");
+                gui.numeros[3].innerHTML = ev.target.value;
+                check4 = true;
+                crono.stop();
+
+            } else {
+                crono.tic();
+            }
+        }
+    }
+
+    for (let boton of gui.digitos) {
+
+        boton.onclick = digito;
     }
