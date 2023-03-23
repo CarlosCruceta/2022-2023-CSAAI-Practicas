b53e5c97f87beb63c1fdc6231333a2bbd517d218
Carlos_Cruceta <c.cruceta.2016@alumnos.urjc.es>
2023-03-23 10:45:44 +0100

    crono

---
+++ P3/TP/index.js
@@ -0,0 +1,134 @@
+const gui = {
+    display : document.getElementById("display"),
+    reset : document.getElementById("reset"),
+  
+}
+class Crono {
+
+    //-- Constructor. Hay que indicar el 
+    //-- display donde mostrar el cronómetro
+    constructor(display) {
+        this.display = display;
+
+        //-- Tiempo
+        this.cent = 0, //-- Centésimas
+        this.seg = 0,  //-- Segundos
+        this.min = 0,  //-- Minutos
+        this.timer = 0;  //-- Temporizador asociado
+    }
+
+    //-- Método que se ejecuta cada centésima
+    tic() {
+        //-- Incrementar en una centesima
+        this.cent += 1;
+
+        //-- 100 centésimas hacen 1 segundo
+        if (this.cent == 100) {
+        this.seg += 1;
+        this.cent = 0;
+        }
+
+        //-- 60 segundos hacen un minuto
+        if (this.seg == 60) {
+        this.min = 1;
+        this.seg = 0;
+        }
+
+        //-- Mostrar el valor actual
+        this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
+    }
+
+    //-- Arrancar el cronómetro
+    start() {
+       if (!this.timer) {
+          //-- Lanzar el temporizador para que llame 
+          //-- al método tic cada 10ms (una centésima)
+          this.timer = setInterval( () => {
+              this.tic();
+          }, 10);
+        }
+    }
+
+    //-- Parar el cronómetro
+    stop() {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+    }
+
+    //-- Reset del cronómetro
+    reset() {
+        this.cent = 0;
+        this.seg = 0;
+        this.min = 0;
+
+        this.display.innerHTML = "0:0:0";
+    }
+}
+
+const crono = new Crono(gui.display);
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas
+canvas.width = 300;
+canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Valor del lado del rectángulo
+let lr = 20;
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
+
+//-- Velocidades del objeto
+let velx = 3;
+let vely = 1;
+
+//-- Función principal de animacion
+function update() 
+{
+  console.log("test");
+  
+  //-- Condicion de rebote en extremos horizontales del canvas
+  if (x < 0 || x >= (canvas.width - lr) ) { 
+    velx = -velx;
+  }
+
+  //-- Condición de rebote en extremos verticales del canvas
+  if (y <= 0 || y > canvas.height- lr) {
+    vely = -vely;
+  }
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+  
+  x = x + velx;
+  y = y + vely;
+
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, lr, lr);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
