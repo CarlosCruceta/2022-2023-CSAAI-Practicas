07b6a44025bc36e0210d354d3542045f5578809d
Carlos_Cruceta <c.cruceta.2016@alumnos.urjc.es>
2023-04-25 09:39:48 +0200

    ej

--- P5/Net/index.js
+++ P5/Net/index.js
@@ -1,56 +1,148 @@
-console.log("Ejecutando JS....")
+console.log("Montando la red...")
+
+const gui = {
+  bsend : document.getElementById("bsend"),
+  netdelay : document.getElementById("netdelay"),
+  netdelayvalue : document.getElementById("netdelay_value"),
+}
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-const img = document.getElementById('imagesrc')
+const imgBack = document.getElementById('imagesrchide');
+const imgCloud = document.getElementById('cloud');
+const imgFront = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 
-//-- Función de retrollamada de imagen cargada
-//-- La imagen no se carga instantaneamente, sino que
-//-- lleva un tiempo. Sólo podemos acceder a ella una vez
-//-- que esté totalmente cargada
-img.onload = function () {
+//-- Gestionar el estado el envío
+const state = {
+  sendingImage: false,
+  totalTime: 0,
+  totalPackages:0,
+  sendingPackage:0,
+  netDelay: 1,
+  netDelayDefault: 1,
+  loop: null
+}
+
+//-- Iniciar el valor del deslizador con el valor de la 
+// variable de estado para el delay
+gui.netdelayvalue.innerHTML = state.netDelay;
+
+//-- Cuando está disponible cargo la imagen con la nube para represntar el destino
+imgCloud.onload = function () {
 
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
-  canvas.width = img.width;
-  canvas.height = img.height;
+  canvas.width = imgCloud.width;
+  canvas.height = imgCloud.height;
 
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavía
-  ctx.drawImage(img, 0,0);
+  ctx.drawImage(imgCloud, 0, 0);
+
+}
+
+//-- función de callback para el envío de la imagen
+gui.bsend.onclick = () => {
+  sendImage()
+}
+
+//-- función de callback para actualizar los valores del 
+// deslizador y la variable de estado para el delay
+gui.netdelay.oninput = () => {
+  gui.netdelayvalue.innerHTML = gui.netdelay.value;
+  state.netDelay = gui.netdelay.value;
+}
+
+//-- simulación del envío de la imagen
+//-- la he planteado como que cada línea horizontal de la imagen
+//-- es un paquete de datos, que sufrirá el retardo correspondiente.
+//-- https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
+const sendImage = () => {
+
+  console.log("Comienzo a enviar...");
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = imgFront.width;
+  canvas.height = imgFront.height;  
+ 
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavía
+  ctx.drawImage(imgFront, 0, 0);
 
   //-- Obtener la imagen del canvas en pixeles
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
+  state.totalPackages = canvas.height;
+  state.sendingImage = true;
+
+  //-- declaro las variables fuera del loop
+  //-- esta me servirá para seleccionar el rectángulo de la imagen
+  let imgData = ctx.getImageData(0, 0, 1, 1)
+  
   //-- Obtener el array con todos los píxeles
   let data = imgData.data
 
-  //-- data[0] es el canal rojo del pixel de la posición 0,0
-  //-- data[1] es el canal verde del pixel 0,0
-  //-- data[2] es el canal azul del pixel 0,0
-  //-- data[3] es el canal de transparencia del pixel 0,0
-  //-- data[4] es el canal ROJO del pixel 1,0
-  //-- ....
-  //-- En general, para el pixel i
-  //-- data[4*i] es el canal rojo
-  //-- data[4*i + 1]: Canal verde
-  //-- data[4*i + 2]: Canal azul
-  //-- data[4*i + 3]: Canal de transparencia
+  state.loop = setInterval(() => {
 
-  //-- Colocar un pixel rojo en (200,50)
+    state.totalTime++
+    state.sendingPackage++
 
-  //-- Obtener el numero de pixel a partir de su posición
-  let i = 200 + 50*canvas.width;
+    //-- dimensiones del rectángulo 1
+    sx1 = 0;
+    sy1 = 0;
+    sw1 = canvas.width;
+    sh1 = state.sendingPackage;
 
-  //-- Pixel rojo: canal rojo a tope. Resto de colores a 0
-  //-- La transparencia no se modifica
-  data[i*4] = 255;    //-- Canal Rojo
-  data[i*4 + 1] = 0;  //-- Canal Verde
-  data[i*4 + 2] = 0;  //-- Canal azul
+    imgData = ctx.getImageData(sx1, sy1, sw1, sh1);
 
-  //-- Poner la imagen modificada en el canvas
-  ctx.putImageData(imgData, 0, 0);
-};
+    //-- Obtener el array con todos los píxeles
+    data = imgData.data
 
-console.log("Fin...");
+    //-- cambiamos el canal a rojo del rectángulo que hemos seleccionado
+    for (let i = 0; i < data.length; i+=4) {
+      data[i] = 0; //-- Canal rojo a 0
+    }
+    
+    //-- dimensiones del rectángulo 2
+    sx2 = sx1;
+    sy2 = sh1+1;
+    sw2 = sw1;
+    sh2 = state.totalPackages - sh1;
+
+    // if (sh2 > 0) {
+    //   //-- seleccionamos el rectángulo 2
+    //   imgData = ctx.getImageData(sx2, sy2, sw2, sh2);
+
+    //   //-- Obtener el array con todos los píxeles
+    //   data = imgData.data
+  
+    //   for (let i = 0; i < data.length; i+=4) {
+    //     // brillo = (3 * r + 4 * g + b)/8
+    //     //let brillo = (3 * data[i] + 4 * data[i+1] + data[i+2])/8
+    //     //data[i] = brillo;
+    //     //data[i+1] = brillo;
+    //     //data[i+2] = brillo; 
+    //     //data[i] = 0;
+    //     //data[i+1] = 0;
+    //     data[i+2] = 0;         
+    //   }  
+    // }
+
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+
+    // Paramos el loop si hemos terminado de enviar
+    if (state.sendingPackage == state.totalPackages) {
+      ctx.drawImage(imgBack, 0, 0); 
+      console.log("Envio terminado...");
+      state.sendingImage = false;             
+      clearInterval(state.loop);
+    }
+
+    console.log("Enviando...");
+  }, state.netDelay )
+}
+
+console.log("Red preparada...");
